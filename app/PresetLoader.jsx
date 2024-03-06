a99b588094d26bff1782896acdfe99c13328017c
kenmoli <46527408+kenmoli@users.noreply.github.com>
2024-03-06 16:07:54 +0800

    Created Icon

--- app/PresetLoader.jsx
+++ app/PresetLoader.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState, useEffect } from "react";
 
 export default function PresetLoader({
@@ -9,19 +7,16 @@ export default function PresetLoader({
   configs,
   setConfigs,
 }) {
-  let configList = JSON.parse(localStorage.getItem("Configurations"));
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    if (configList) {
-      setConfigs(configList);
-      haveConfig.current = true;
-    }
+    let configList = JSON.parse(localStorage.getItem("Maze Presets"));
+    setConfigs(configList);
+    haveConfig.current = true;
   }, []);
 
   useEffect(() => {
     if (haveConfig.current) {
-      configList = configs;
       setCounter(counter + 1);
     }
   }, [configs]);
@@ -33,14 +28,14 @@ export default function PresetLoader({
 
   return (
     <>
-      {configList && (
+      {configs && (
         <div className="bg-white rounded-xl shadow-xl mb-8 p-4 w-full max-w-4xl">
           <div className="card-body items-center text-center p-4">
             <h2 className="mb-5 text-xl font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
               Configurations
             </h2>
             <div className="container flex-col">
-              {Object.entries(configList).map(([key, value]) => (
+              {Object.entries(configs).map(([key, value]) => (
                 <div key={key} className="container flex justify-evenly p-2">
                   <div className="w-1/2 flex items-center border-b-2 border-gray-300">
                     <p className="font-bold text-black text-center">{key}</p>
